f77f9ae94751842c0f46bde485ad76ac90277083
Matthew Brighty <MBrighty@users.noreply.github.com>
2021-04-04 21:14:58 +0100

    Add Bandit (#7)
    * Add bandit dependency
    
    * Remove hardcoded binding to all interfaces
    
    * Add bandit to CI runs

--- pynab_monzo/__main__.py
+++ pynab_monzo/__main__.py
@@ -4,7 +4,4 @@ from .app import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    host = "0.0.0.0"
-    if port == 5000:
-        host = "127.0.0.1"
-    app.run(host=host, port=port)
+    app.run(port=port)
