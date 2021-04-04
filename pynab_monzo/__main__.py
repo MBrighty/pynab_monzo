d3b0c51ac9fb791860f5954e5a82d292cbda2db7
Matthew Brighty <MBrighty@users.noreply.github.com>
2021-04-04 20:57:16 +0100

    Remove setting of debug config value in code

--- pynab_monzo/__main__.py
+++ pynab_monzo/__main__.py
@@ -5,8 +5,6 @@ from .app import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     host = "0.0.0.0"
-    debug = False
     if port == 5000:
         host = "127.0.0.1"
-        debug = True
-    app.run(host=host, port=port, debug=debug)
+    app.run(host=host, port=port)
